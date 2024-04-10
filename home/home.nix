864f1d1fb82c652e7fae10d7c79210af68620d8e
duli <duli4868@gmail.com>
2024-04-10 19:49:19 +0800

    chore: auto-optimise

--- home/home.nix
+++ home/home.nix
@@ -24,6 +24,7 @@
   nix = {
     package = pkgs.nix;
     settings.experimental-features = [ "nix-command" "flakes" ];
+    settings.auto-optimise-store = true;
   };
 
   programs.home-manager.enable = true;
