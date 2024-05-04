be3dce15ee1649c0795ff87965384e5090ac7863
duli <duli4868@gmail.com>
2024-05-04 20:05:35 +0800

    pkg: librime-with-plugins

---
+++ pkgs/librime-with-plugins/default.nix
@@ -0,0 +1,32 @@
+{
+  librime,
+  lua54Packages,
+  source-librime-charcode,
+  source-librime-lua,
+  source-librime-octagram,
+  source-librime-proto,
+  ...
+}:
+(librime.override {
+  plugins = [
+    (source-librime-charcode.src.overrideAttrs (old: {
+      name = "librime-charcode";
+    }))
+    (source-librime-lua.src.overrideAttrs (old: {
+      name = "librime-lua";
+    }))
+    (source-librime-octagram.src.overrideAttrs (old: {
+      name = "librime-octagram";
+    }))
+    (source-librime-proto.src.overrideAttrs (old: {
+      name = "librime-proto";
+    }))
+  ];
+}).overrideAttrs
+  (old: {
+    buildInputs = (old.buildInputs or [ ]) ++ [ lua54Packages.lua ];
+
+    meta = old.meta // {
+      description = "Librime with plugins (librime-charcode, librime-lua, librime-octagram, librime-proto)";
+    };
+  })
