65e37e6d909f17f00e41c5ac795a42088a171b0a
duli <duli4868@gmail.com>
2024-05-04 21:35:44 +0800

    refactor(mapPackage): more pretty?

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,19 +1,27 @@
-rec {
+with builtins;
+let
   mapPackages =
-    f:
-    with builtins;
-    listToAttrs (
-      map
-        (name: {
-          inherit name;
-          value = f name;
-        })
-        (
-          filter (v: v != null) (
-            attrValues (mapAttrs (k: v: if v == "directory" && k != "_sources" then k else null) (readDir ./.))
-          )
-        )
-    );
+    let
+      filterDir =
+        dir:
+        (filter
+          (v: v != null)
+          (attrValues
+            (mapAttrs
+              (k: v:
+                if v == "directory" && k != "_sources"
+                then k
+                else null)
+              dir)));
+    in
+    callPackage:
+    (listToAttrs
+      (map
+        (name: { inherit name; value = (callPackage name); })
+        (filterDir (readDir ./.))));
+in
+rec {
+  inherit mapPackages;
   packages = pkgs: mapPackages (name: pkgs.${name});
   overlay =
     final: prev:
