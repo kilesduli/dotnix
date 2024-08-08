1e9581a9c1b96868fbedd23081028bc26d25917d
duli <duli4868@gmail.com>
2024-08-08 15:24:16 +0800

    package(calibre): replace the way to customize calibre

--- nix-pkgs/calibre-enhanced/default.nix
+++ nix-pkgs/calibre-enhanced/default.nix
@@ -1,23 +1,9 @@
 { calibre
-, source-calibre-do-not-translate
 , lib
-, python3Packages
 , ...
 }:
-let
-  calibre-do-not-translate = source-calibre-do-not-translate.src;
-in
 calibre.overrideAttrs (old: {
-  postPatch = lib.optionalString (old ? postPatch) "" + ''
-    echo "[=] Patch backend.py"
-    cp -r ${calibre-do-not-translate} ./patch-script/
-    chmod +w ./patch-script
-    python ./patch-script/patch_backend.py --os unix src/calibre/db/backend.py ./patch-script/backend.py
-    cp ./patch-script/backend.py src/calibre/db/backend.py
-  '';
-
-  buildInputs = old.buildInputs ++ [
-    python3Packages.click
-    python3Packages.sqlparse
+  patches = old.patches ++ [
+    ./0001-feat-avoid-latinization-of-paths-in-certain-function.patch
   ];
 })
