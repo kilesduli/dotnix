920b7cc8a2844f00baece237e94d98ca8c8c106c
duli <duli4868@gmail.com>
2024-09-28 15:16:13 +0800

    package(sioyek-patched): switch to development branch

--- nix-pkgs/sioyek-patched/default.nix
+++ nix-pkgs/sioyek-patched/default.nix
@@ -1,16 +1,87 @@
-{ sioyek, fetchurl, fetchFromGitHub, lib, ... }:
+{ qt6 }:
+let
+  fn =
+    { lib
+    , ccacheStdenv
+    , installShellFiles
+    , fetchFromGitHub
+    , freetype
+    , gumbo
+    , harfbuzz
+    , jbig2dec
+    , mujs
+    , mupdf
+    , openjpeg
+    , qt3d
+    , qtbase
+    , qmake
+    , qtspeech
+    , wrapQtAppsHook
+    }:
+    ccacheStdenv.mkDerivation (finalAttrs: {
+      pname = "sioyek";
+      version = "2.0.0-unstable-2024-09-27";
 
-sioyek.overrideAttrs (
-  old: {
-    src = fetchFromGitHub {
-      owner = "ahrm";
-      repo = "sioyek";
-      rev = "7181c5a463ed4cbeda7ab4937511bdfc836d6c94";
-      sha256 = "sha256-lHKB9jchsRYjgF7KJdlDTpKVZGP8lq1btmTt3ud67wM=";
-    };
-    patches = (fetchurl {
-      url = "https://gist.githubusercontent.com/tennysontbardwell/70bac2f20946e9609686866faa188aea/raw/d84c577d58bc37436147d855984f5a66a4e0988a/mupdf-0.23.0-rev-277aed7.patch";
-      hash = "sha256-Gajno8+qdWIuYQWiu0/yY2YzJ6nR97pkjwhY1bEflIA=";
+      src = fetchFromGitHub {
+        owner = "ahrm";
+        repo = "sioyek";
+        rev = "ae733e927a4bddc49abe81b96a1d96361736ea52";
+        hash = "sha256-kErb8PZmp670D7/+oJfKllVD/ypgNNFdx74kIaYiXsw=";
+      };
+
+      buildInputs = [
+        gumbo
+        harfbuzz
+        jbig2dec
+        mujs
+        mupdf
+        openjpeg
+        qt3d
+        qtbase
+        qtspeech
+      ]
+      ++ lib.optionals ccacheStdenv.isDarwin [ freetype ];
+
+      nativeBuildInputs = [
+        installShellFiles
+        qmake
+        wrapQtAppsHook
+      ];
+
+      qmakeFlags = lib.optionals ccacheStdenv.isDarwin [ "CONFIG+=non_portable" ];
+
+      postPatch = ''
+        substituteInPlace pdf_viewer_build_config.pro \
+          --replace "-lmupdf-threads" "-lgumbo -lharfbuzz -lfreetype -ljbig2dec -ljpeg -lopenjp2" \
+          --replace "-lmupdf-third" ""
+        substituteInPlace pdf_viewer/main.cpp \
+          --replace "/usr/share/sioyek" "$out/share" \
+          --replace "/etc/sioyek" "$out/etc"
+      '';
+
+      postInstall =
+        if ccacheStdenv.isDarwin then ''
+          cp -r pdf_viewer/shaders sioyek.app/Contents/MacOS/shaders
+          cp pdf_viewer/{prefs,prefs_user,keys,key_user}.config tutorial.pdf sioyek.app/Contents/MacOS/
+
+          mkdir -p $out/Applications $out/bin
+          cp -r sioyek.app $out/Applications
+          ln -s $out/Applications/sioyek.app/Contents/MacOS/sioyek $out/bin/sioyek
+        '' else ''
+          install -Dm644 tutorial.pdf $out/share/tutorial.pdf
+          cp -r pdf_viewer/shaders $out/share/
+          install -Dm644 -t $out/etc/ pdf_viewer/{keys,prefs}.config
+          installManPage resources/sioyek.1
+        '';
+
+      meta = with lib; {
+        homepage = "https://sioyek.info/";
+        description = "A PDF viewer designed for research papers and technical books";
+        changelog = "https://github.com/ahrm/sioyek/releases/tag/v${finalAttrs.version}";
+        license = licenses.gpl3Only;
+        maintainers = with maintainers; [ podocarp xyven1 ];
+        platforms = platforms.unix;
+      };
     });
-  }
-)
+in
+qt6.callPackage fn {}
