dec9fb68db5605fed5f3a7c2d45233ddf10c5d17
duli <duli4868@gmail.com>
2024-10-28 17:30:16 +0800

    package(emacs): avoid use program.emacs, reason list below
    * program.emacs will call emacsWithPackages that will create a
    derivation that only contains `bin` and `share`. and there is no other
    way to avoid, except create pr.

--- nix-home/part/emacs.nix
+++ nix-home/part/emacs.nix
@@ -1,17 +1,23 @@
 { self, config, lib, pkgs, ... }:
 
+let
+  emacs = pkgs.emacs-master;
+in
 {
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs-master;
-  };
+  # the programs.emacs has warp that cause no include dir
+  home.packages = [ emacs ];
 
   services.emacs = {
     enable = true;
+    package = emacs;
   };
 
   home.file.".emacs.d" = {
     source = self.inputs.doom-emacs.outPath;
     recursive = true;
+  };
+
+  home.sessionVariables = {
+    LSP_USE_PLISTS = "true";
   };
 }
