8b310caa1f6057dbfad517ec895567ce80e42e3c
duli <duli4868@gmail.com>
2024-11-28 20:45:32 +0800

    package(emacs-master): switch to gtk version

--- nix-pkgs/emacs-master/default.nix
+++ nix-pkgs/emacs-master/default.nix
@@ -4,7 +4,7 @@
 , ccacheStdenv
 , ...
 }:
-(emacs-git.override { stdenv = ccacheStdenv; }).overrideAttrs (
+(emacs-git.override { stdenv = ccacheStdenv; withGTK3 = true; }).overrideAttrs (
   old: {
     buildInputs = lib.lists.remove xorg.libXi old.buildInputs;
     configureFlags = lib.lists.remove "--with-xinput2" old.configureFlags ++ [ "--without-xim" ];
