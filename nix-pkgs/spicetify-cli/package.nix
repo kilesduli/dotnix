022366ad8239a419c09bab8cf4bc8265ef331ad3
duli <duli4868@gmail.com>
2025-09-29 19:54:33 +0800

    pkgs(spicetify-cli): fix failing patch by using substituteInPlace

--- nix-pkgs/spicetify-cli/package.nix
+++ nix-pkgs/spicetify-cli/package.nix
@@ -5,8 +5,15 @@
 let
   removePrefixv = version: with builtins; substring 1 (stringLength version) version;
 in
-spicetify-cli.overrideAttrs (old: {
+spicetify-cli.overrideAttrs (old: rec {
   version = removePrefixv source-spicetify-cli.version + "-unstable";
   inherit (source-spicetify-cli) src;
   vendorHash = "sha256-iD6sKhMnvc0RczoSCWCx/72/zjoC6YQyV+AYyE4w/b0=";
+  patches = [ ];
+
+  postPatch = (old.postPatch or "") + ''
+    substituteInPlace src/preprocess/preprocess.go --replace-warn \
+    "	if version != \"Dev\" {" \
+    "	if version != \"${version}\" {"
+  '';
 })
