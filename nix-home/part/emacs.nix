caee2d375628e40f5248f51b38824fce299b77b2
duli <duli4868@gmail.com>
2026-01-01 12:59:11 +0800

    emacs: switch to master

--- nix-home/part/emacs.nix
+++ nix-home/part/emacs.nix
@@ -1,7 +1,7 @@
 { self, config, lib, pkgs, ... }:
 
 let
-  emacs = pkgs.emacs-master-lucid-w_igc-wo_cairo-wo_native-comp;
+  emacs = pkgs.emacs-master-lucid-wo_igc-wo_cairo-wo_native-comp;
 in
 {
   # the programs.emacs has warp that cause no include dir
@@ -10,5 +10,8 @@ in
   services.emacs = {
     enable = true;
     package = emacs;
+    # seems I don't use daemon in tty 
+
+    startWithUserSession = "graphical";
   };
 }
